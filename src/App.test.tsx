532daeb83c7b294a31a77aa0a7b8e8ea88300606
RONNY FREITES <ronnyangelo.freites@gmail.com>
2020-03-05 15:42:18 -0600

    add enzyme library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
 import App from './App';
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
